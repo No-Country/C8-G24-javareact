4856a78d8bcc5c69d112c08008f6fd90950d3507
sebastian barcia <sebastianbarcia@hotmail.com>
2023-02-10 09:45:49 +0100

    cambio

--- frontend/src/views/catalog/Catalog.jsx
+++ frontend/src/views/catalog/Catalog.jsx
@@ -5,22 +5,21 @@ const Catalogo = () => {
   return (
     <div>
       <ul className="bg-[#EADCDC] grid grid-cols-4 gap-4 text-center p-16">
-      {records.map((item) => {
-             
-             return (
-               <>
-                 <Card1
-                   image={item.img}
-                   description={item.product}
-                   comercios={item.comercios}
-                   id = {item.id}
-                   shopping={item.comercios.map((item) => item.negocio)[0]}
-                   shipments={item.comercios.map((item) => item.envio)[0]}
-                   price={item.comercios.map((item) => item.precio)[0]}
-                 />
-               </>
-             );
-           })}
+        {records.map((item) => {
+          return (
+            <>
+              <Card1
+                image={item.img}
+                description={item.product}
+                comercios={item.comercios}
+                id={item.id}
+                shopping={item.comercios.map((item) => item.negocio)[0]}
+                shipments={item.comercios.map((item) => item.envio)[0]}
+                price={item.comercios.map((item) => item.precio)[0]}
+              />
+            </>
+          );
+        })}
       </ul>
     </div>
   );
